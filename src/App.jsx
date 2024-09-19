038947daa6438d5040d8e1840a9fc670fcf52eab
Luis <luisbarrios0110@gmail.com>
2024-09-18 22:45:46 -0500

    Definiendo diseño final de tabla con TailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import "./index.css";
 
 function Button (props) {
   return (
-    <button className='bg-blue-900 hover:bg-blue-500 text-white'> {props.numero}</button>
+      <button className="w-28 h-28 bg-white text-black text-5xl font-bold border border-sky-500 hover:bg-sky-50">{props.numero}</button>
   )  
 }
 
@@ -11,15 +12,27 @@ function App() {
 
   return (
     <>
-      <div>
-        <Button numero="1"></Button>
-        <Button numero="2"></Button>
-        <Button numero="3"></Button>
-      </div>
+      <div className=''>
+        <h1 className='flex items-center justify-center font-bold text-7xl font-serif mt-9'>Tic Tac Toe Game</h1>
+        
+        <div class="flex justify-center items-center min-h-[60vh]">
+          <div className="grid grid-cols-3 bg-white shadow-lg shadow-blue-300 rounded-lg ">
+            <Button numero="X"></Button>
+            <Button numero="O"></Button>
+            <Button numero="X"></Button>
+            <Button numero="O"></Button>
+            <Button numero="X"></Button>
+            <Button numero="O"></Button>
+            <Button numero="X"></Button>
+            <Button numero="O"></Button>
+            <Button numero=""></Button>
+          </div>
+        </div>
+
+      </div>  
     </>
   )
 }
-
 
 export default App
 
